6ff7ab715f00b3c074cf36a96dbac5bb27684e95
Nick Molski <nam3vy@mail.missouri.edu>
2019-03-08 15:49:59 -0600

    First commit

---
+++ Pets/Pets/Cat.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Cat
+    {
+        public Cat(string name, string owner, double weight)
+        {
+            Name = name;
+            Owner = owner;
+            Weight = weight;
+        }
+
+        public string Name { get; }
+        public string Owner { get; }
+        public double Weight { get; }
+
+        public string getTag()
+        {
+            string tag = "If lost, call " + Owner;
+            return tag;
+        }
+
+
+
+
+
+        public string meow(int count)
+        {
+            int i = 1;
+            string meowLine = "meow.";
+            string numOfMeows = "";
+            while (i <= count)
+            {
+                numOfMeows = numOfMeows + meowLine;
+                i++;
+            }
+            return numOfMeows;
+        }
+    }
+}
---
+++ Pets/Pets/Dog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets
+{
+    class Dog
+    {
+        public Dog(string name, string owner, double weight)
+        {
+            Name = name;
+            Owner = owner;
+            Weight = weight;
+        }
+
+        public string pType { get; }
+        public string Name { get; }
+        public string Owner { get; }
+        public double Weight { get; }
+
+        public string getTag()
+        {
+            string tag = "If lost, call " + Owner;
+            return tag;
+        }
+
+
+
+
+
+        public string bark(int count)
+        {
+            int i = 1;
+            string barkLine = "bark!";
+            string numOfBarks = "";
+            while (i <= count)
+            {
+                numOfBarks = numOfBarks + barkLine;
+                i++;
+            }
+            return numOfBarks;
+        }
+    }
+}
